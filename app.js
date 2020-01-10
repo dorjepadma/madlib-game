2bc1d83b3f339de71f6c519a5834b19261e51bda
dorjepadma <dorjepadma@gmail.com>
2020-01-09 22:56:04 -0800

    madlib2

---
+++ app.js
@@ -0,0 +1,23 @@
+const adj1input = document.getElementById ('adj1input');
+const adj1Output = adj1input.value;
+adj1input.textContent = adj1output;
+
+const noun1input = document.getElementById ('noun1input');
+const noun11Output = noun1input.value;
+noun1input.textContent = noun1output;
+
+const color1input = document.getElementById ('color1input');
+const color1input = color1input.value;
+color1input.textContent = color1output;
+
+const noun2input = document.getElementById ('noun2input');
+const noun21Output = noun2input.value;
+noun2input.textContent = noun2output;
+
+const adj2input = document.getElementById ('adj2input');
+const ad21Output = adj2input.value;
+adj2input.textContent = adj2output;
+
+const animalinput = document.getElementById ('animalinput');
+const animalOutput = animal1input.value;
+animalinput.textContent = animaloutput;
